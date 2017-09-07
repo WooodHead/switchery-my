cd47c38d0fedfe55beacfa9c04da818040edae43
WooodHead <WooodHead@users.noreply.github.com>
2017-09-07 14:32:07 +0800

    add click event

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 window.addEventListener('load', function () {
   var switchery = document.getElementsByClassName('switchery')[0]
+  var checkbox = document.getElementsByClassName('js-switch')[0]
   switchery.addEventListener('click', function (e) {
     console.log('click')
     console.log('switchery', switchery)
-    switchery.classList.add('active')
+    if (switchery.classList.contains('active')) {
+      switchery.classList.remove('active')
+      checkbox.checked = false
+    } else {
+      switchery.classList.add('active')
+      checkbox.checked = true
+    }
   }, true)
 })
